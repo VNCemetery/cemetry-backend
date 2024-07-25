e9d43f6fd8857649649db5f0dd98ad6792ae6925
Nguyen Huu Loc <locn562836@gmail.com>
2024-07-25 10:23:58 +0700

    first commit

---
+++ src/main/java/org/dongthap/lietsi/WeblietsiBackendApplication.java
@@ -0,0 +1,13 @@
+package org.dongthap.lietsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblietsiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeblietsiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dongthap/lietsi/WeblietsiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dongthap.lietsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblietsiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
