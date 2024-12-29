812f688115d8b9991bcc4e53bf43441ee020b20c
Nguyen Huu Loc <locn562836@gmail.com>
2024-12-30 06:00:21 +0700

    add testing user

---
+++ src/main/resources/data/V1.0.4__add_testing_users.sql
@@ -0,0 +1,1 @@
+INSERT INTO public.users VALUES ('8499477e-2580-431b-8418-99465878f32e', '2024-12-26 08:13:59.364232', '2024-12-26 08:13:59.364232', 'hovietanh147@gmail.com', '$2a$12$vN.fbI69UOW7pKYDglW7vurfnFGm3PL4ou/YwOa3QCpVIz/chqqfG', 'vietanh');
